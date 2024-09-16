aaa1e776ff32da0b742daf16fcbf3c86f2f83530
goular7 <gustavogoulart1970@gmail.com>
2024-09-16 07:18:56 -0300

    start-project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./componentes/container/container.component";
+import { HeaderComponent } from './componentes/header/header.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    ContainerComponent, 
+    HeaderComponent, 
+    SeparadorComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }
