259672eb36abf8ca1dd0b7ec4ff1f6d6012185c1
goular7 <gustavogoulart1970@gmail.com>
2024-09-17 07:18:48 -0300

    contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./componentes/container/container.component";
 import { HeaderComponent } from './componentes/header/header.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
+
 
 @Component({
   selector: 'app-root',
@@ -13,11 +23,19 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     RouterOutlet,
     ContainerComponent, 
     HeaderComponent, 
-    SeparadorComponent
+    SeparadorComponent,
+    ContatoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+
+  filtrarContratosPorLetraInicial(letra:string) : Contato[] {
+    return this.contatos.filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    } )
+  }
 }
